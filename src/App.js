3f3a71a0ee6f7727f4c67b98056ffc8552c196c1
Muthulakshmi Murugan <muthulakshmi.murugan@aspiresys.com>
2023-03-09 12:31:36 +0530

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import './App.css';
 import Gallery from './state';
 import Clock from './lifecycle';
 import Toggle from './events';
+import LoginControl from './conditionalRendering';
+import Mailbox from './inlineIfConditional';
 
  function AppTest() {
+  const messages = ['React', 'Re: React', 'Re:Re: React'];
   return (
     /* <div id="root">         
     </div>  */
@@ -12,6 +15,8 @@ import Toggle from './events';
       <Gallery />
       <Clock />
       <Toggle />
+      <LoginControl />
+      <Mailbox unreadMessages={messages} />
     </div>
 
       
