20a2e9778b87e9dc151abc28ae1006e6ba856375
Muthulakshmi Murugan <muthulakshmi.murugan@aspiresys.com>
2023-03-09 16:08:00 +0530

    react forms

--- src/App.js
+++ src/App.js
@@ -6,19 +6,24 @@ import Toggle from './events';
 import LoginControl from './conditionalRendering';
 import Mailbox from './inlineIfConditional';
 import List from './renderingList';
+import MyForm from './forms';
+import EssayForm from './textarea';
+import FlavorForm from './select';
 
  function AppTest() {
   const messages = ['React', 'Re: React', 'Re:Re: React'];
   return (
-    /* <div id="root">         
-    </div>  */
+    
     <div id="root">
       <Gallery />
       <Clock />
       <Toggle />
       <LoginControl />
       <Mailbox unreadMessages={messages} />
-    <List />
+      <List />
+      <MyForm />
+     <EssayForm /> 
+    <FlavorForm />
     </div>
 
       
